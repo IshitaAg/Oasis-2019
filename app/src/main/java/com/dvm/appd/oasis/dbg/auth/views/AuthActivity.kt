c6d553e88ae45b74986e4e105e8799e2e188676f
ishita Aggarwal <e.agrawal03@gmail.com>
2019-10-11 21:54:21 +0530

    Minor changes

--- app/src/main/java/com/dvm/appd/oasis/dbg/auth/views/AuthActivity.kt
+++ app/src/main/java/com/dvm/appd/oasis/dbg/auth/views/AuthActivity.kt
@@ -25,6 +25,7 @@ import com.google.android.gms.common.api.ApiException
 import kotlinx.android.synthetic.main.activity_auth.*
 import kotlinx.android.synthetic.main.activity_auth.password
 import kotlinx.android.synthetic.main.activity_auth.username
+import kotlinx.android.synthetic.main.activity_picture.*
 
 
 class AuthActivity : AppCompatActivity() {
@@ -51,10 +52,8 @@ class AuthActivity : AppCompatActivity() {
                     Toast.makeText(this, "Please enter all fields", Toast.LENGTH_SHORT).show()
                 else -> {
 
-                   progress.visibility = View.VISIBLE
-                    authViewModel.login(username.text.toString(), password.text.toString())
 
-                    // loadingPbr.visibility = View.VISIBLE
+                    authViewModel.login(username.text.toString(), password.text.toString())
                     outsteeLogin.setBackgroundColor(Color.parseColor("#00000000"))
                     CircularLoadingButton.startAnimation()
                     authViewModel.login(username.text.toString(),password.text.toString())
@@ -72,24 +71,24 @@ class AuthActivity : AppCompatActivity() {
         authViewModel.state.observe(this, Observer {
             when (it!!) {
                 LoginState.MoveToMainApp -> {
-                    progress.visibility = View.GONE
+
                     startActivity(Intent(this, MainActivity::class.java))
                     finish()
                 }
                 LoginState.MoveToOnBoarding -> {
-                    progress.visibility = View.GONE
+
                     startActivity(Intent(this, OnboardingActivity::class.java))
                     finish()
                 }
                 is LoginState.Failure -> {
-                    // loadingPbr.visibility = View.GONE
+
                     outsteeLogin.background = resources.getDrawable(R.drawable.add_button_profile)
                     CircularLoadingButton.revertAnimation()
                     Toast.makeText(this, (it as LoginState.Failure).message, Toast.LENGTH_LONG)
                         .show()
                 }
                 LoginState.MoveToPic -> {
-                    progress.visibility = View.GONE
+
                     startActivity(Intent(this,PictureActivity::class.java))
 
                 }
@@ -112,7 +111,6 @@ class AuthActivity : AppCompatActivity() {
                 val profile = GoogleSignIn.getSignedInAccountFromIntent(data)
                     .getResult(ApiException::class.java)
                 Toast.makeText(this, profile!!.displayName, Toast.LENGTH_SHORT).show()
-                progress.visibility = View.VISIBLE
                 authViewModel.Blogin(profile.idToken!!)
             } catch (e: ApiException) {
                 Log.d("checke", e.toString())

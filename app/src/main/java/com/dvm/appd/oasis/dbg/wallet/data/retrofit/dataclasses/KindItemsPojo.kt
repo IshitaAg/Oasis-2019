4f50e29c1c3be5819f00c35bc1f86e432ef20dbd
ishita Aggarwal <e.agrawal03@gmail.com>
2019-10-13 03:47:03 +0530

    Minor changes

--- app/src/main/java/com/dvm/appd/oasis/dbg/wallet/data/retrofit/dataclasses/KindItemsPojo.kt
+++ app/src/main/java/com/dvm/appd/oasis/dbg/wallet/data/retrofit/dataclasses/KindItemsPojo.kt
@@ -2,4 +2,4 @@ package com.dvm.appd.oasis.dbg.wallet.data.retrofit.dataclasses
 
 import com.dvm.appd.oasis.dbg.wallet.data.room.dataclasses.KindItems
 
-data class KindItemsPojo(val items:Map<String,KItemDetailsPojo>)
+data class KindItemsPojo(val items:List<String>)
